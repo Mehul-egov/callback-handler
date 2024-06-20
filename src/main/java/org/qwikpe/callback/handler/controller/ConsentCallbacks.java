9eb3a2865152ae19a003297c92e5bcd5082e4109
hiten-qwikpe <hiten.khatri@qwikpe.in>
2024-06-20 18:49:10 +0530

    added uhi callbacks.

--- src/main/java/org/qwikpe/callback/handler/controller/ConsentCallbacks.java
+++ src/main/java/org/qwikpe/callback/handler/controller/ConsentCallbacks.java
@@ -7,6 +7,7 @@ import org.qwikpe.callback.handler.service.ConsentCallbacksService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.convert.PeriodUnit;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
---
+++ src/main/java/org/qwikpe/callback/handler/controller/TestController.java
@@ -0,0 +1,20 @@
+package org.qwikpe.callback.handler.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/v0.5/testing")
+    public ResponseEntity<Object> test() {
+        LOGGER.info("Welcome to test api of callback handler !!");
+        return ResponseEntity.ok("Welcome to test api of callback handler !!");
+    }
+
+}
